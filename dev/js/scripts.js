6a10a9ae57be2d556f0ee2c960cac399757a38cc
JohnnyLau1028 <81736997+JohnnyLau1028@users.noreply.github.com>
2021-10-23 21:37:30 -0400

    preloader set up

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,11 +2,23 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from(".hero", {duration:2, alpha:0, y:"-=1000", scale:2})
-.from(".hero h1",{duration:3, alpha:0, x:"+=1000"}, "-=2");
 
-mainTL.from("aside", {duration:.5, alpha:0, scale:5});
 
-mainTL.from("#visitus", {duration:.5, alpha:0, scale:3, y:"+=1000"}, "-=1");
 
-mainTL.from("p", {duration:.5, alpha:0, y:"+=100"});
+
+
+
+
+
+
+
+//const mainTL = gsap.timeline();
+
+//mainTL.from(".hero", {duration:2, alpha:0, y:"-=1000", scale:2})
+//.from(".hero h1",{duration:3, alpha:0, x:"+=1000"}, "-=2");
+
+//mainTL.from("aside", {duration:.5, alpha:0, scale:5});
+
+//mainTL.from("#visitus", {duration:.5, alpha:0, scale:3, y:"+=1000"}, "-=1");
+
+//mainTL.from("p", {duration:.5, alpha:0, y:"+=100"});
