080e16dd246603c4a930f8f64964b7c4bbc3bd9e
JohnnyLau1028 <81736997+JohnnyLau1028@users.noreply.github.com>
2021-10-31 19:50:04 -0400

    updated gsap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,10 +1,19 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ExpoScaleEase, RoughEase, SlowMo } from "gsap/EasePack";
+import { EaselPlugin } from "gsap/EaselPlugin"; 
 
-gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin (GSDevTools, DrawSVGPlugin, EaselPlugin, ExpoScaleEase, RoughEase, SlowMo);
 
 const mainTL = gsap.timeline();
 
+function cup (){
+const TL = gsap.timeline();
+gsap.to("#cup", { duration: 2, ease: "power3.out", x: "-800%" });
+return TL;
+}
+
+mainTL.add(cup);
 
 
 
@@ -25,3 +34,5 @@ const mainTL = gsap.timeline();
 //mainTL.from("#visitus", {duration:.5, alpha:0, scale:3, y:"+=1000"}, "-=1");
 
 //mainTL.from("p", {duration:.5, alpha:0, y:"+=100"});
+
+GSDevTools.create();
