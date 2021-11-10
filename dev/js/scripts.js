275e3d32b9fb5297881bddbe96abf66a7bd06679
JohnnyLau1028 <81736997+JohnnyLau1028@users.noreply.github.com>
2021-11-10 01:02:34 -0500

    preloader finished

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -19,7 +19,7 @@ function cup(){
 function circle(){
     const tl= gsap.timeline();
 
-    tl.from("#circle", {duration:1.5, drawSVG: "0%"})
+    tl.from("#circle", {duration:1, drawSVG: "0%"})
     tl.to("#circle", {alpha:0})
 
     return tl;
@@ -37,7 +37,7 @@ function halfmoon(){
 function progress1(){
     const tl= gsap.timeline();
 
-    tl.from("#progress1", {duration:2, scaleY:0})
+    tl.from("#progress1", {duration:1, scaleY:0})
 
     return tl;
 }
@@ -45,7 +45,7 @@ function progress1(){
 function progress3(){
     const tl= gsap.timeline();
 
-    tl.from("#progress3", {duration:2, scaleY:0})
+    tl.from("#progress3", {duration:.75, scaleY:0})
 
     return tl;
 }
@@ -53,8 +53,28 @@ function progress3(){
 function progress2(){
     const tl= gsap.timeline();
 
-    tl.from("#progress2", {duration:2, scaleY:5})
-    tl.from("#ellipse", {scale:.1, y:"100"})
+    tl.from("#progress2", {duration:2, alpha:0, scaleX:.5, scaleY:.1, y:"70", x:"25"}, "same")
+    tl.from("#ellipse", {duration:2, alpha:0, scaleX:.5, scaleY:.1, y:"70", x:"25"}, "same")
+    tl.to("#progress1", {duration:.75, scaleY:0})
+
+    return tl;
+}
+
+function coffee(){
+    const tl= gsap.timeline();
+
+    tl.to(".coffee", {duration:1, y:"1000"})
+    tl.to("#halfmoon", {duration:.5, alpha:0})
+    tl.to("#circle", {duration:.5, alpha:1})
+
+    return tl;
+}
+
+function transition(){
+    const tl= gsap.timeline();
+
+    tl.to("#circle",{duration:2, scale:25, fill:"#705637", stroke:"none"});
+    tl.to("#preloader",{duration:2, autoAlpha:0},"-=2");
 
     return tl;
 }
@@ -64,25 +84,17 @@ mainTL.add(cup())
         .add(halfmoon())
         .add(progress1())
         .add(progress3())
-        .add(progress2());
-
-
-
-
-
-
-
-
+        .add(progress2())
+        .add(coffee())
+        .add(transition());
 
 GSDevTools.create();
 
-//const mainTL = gsap.timeline();
+mainTL.from(".hero", {duration:1, alpha:0, y:"-=1000", scale:2}, "-=2")
+.from(".hero h1",{duration:3, alpha:0, x:"+=1000"}, "-=2");
 
-//mainTL.from(".hero", {duration:2, alpha:0, y:"-=1000", scale:2})
-//.from(".hero h1",{duration:3, alpha:0, x:"+=1000"}, "-=2");
+mainTL.from("aside", {duration:.5, alpha:0, scale:5});
 
-//mainTL.from("aside", {duration:.5, alpha:0, scale:5});
+mainTL.from("#visitus", {duration:.5, alpha:0, scale:3, y:"+=1000"}, "-=1");
 
-//mainTL.from("#visitus", {duration:.5, alpha:0, scale:3, y:"+=1000"}, "-=1");
-
-//mainTL.from("p", {duration:.5, alpha:0, y:"+=100"});
+mainTL.from("p", {duration:.5, alpha:0, y:"+=100"});
